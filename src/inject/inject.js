c287b3bc97dc904bb44634fa2b3b75a463365505
Wakim <wakim.jraige@gmail.com>
2020-01-11 15:58:31 -0300

    Fix typo

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -45,7 +45,7 @@ const createFilterDOM = () => {
 
     wrapper.className = 'wrapper__filter';
 
-    input.placeholder = 'Entre os fundos separado por vírgula';
+    input.placeholder = 'Entre com os fundos separado por vírgula';
     input.className = 'input__filter';
     input.onkeyup = (event) => handleKeyUp(event);
 
